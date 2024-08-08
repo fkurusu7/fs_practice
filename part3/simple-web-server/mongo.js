3fd69e113005a29841f6151de31e51d2a17fb2be
fkurusu7 <coding.fcv@gmail.com>
2024-08-08 00:33:50 -0600

    add mongoose and test connect to mongodb

---
+++ part3/simple-web-server/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://admin:${password}@cluster0-fs.qnjdn.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "Html is pretty easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved");
+  mongoose.connection.close();
+});
