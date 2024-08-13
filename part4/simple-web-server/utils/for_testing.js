1e87256dba22e95aafc8f3a4c61888c3f66e5b01
fkurusu7 <coding.fcv@gmail.com>
2024-08-12 23:40:10 -0600

    part4 add unit tests with node test

---
+++ part4/simple-web-server/utils/for_testing.js
@@ -0,0 +1,10 @@
+const reverse = (str) => str.split("").reverse().join("");
+
+const average = (array) => {
+  if (array.length === 0) return 0;
+
+  const reducer = (sum, item) => sum + item;
+  return array.reduce(reducer, 0) / array.length;
+};
+
+module.exports = { reverse, average };
