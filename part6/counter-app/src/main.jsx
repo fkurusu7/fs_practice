60e5820d4454e66516af2f1b5b55e2d8b27c3b49
fkurusu7 <coding.fcv@gmail.com>
2024-09-08 22:49:28 -0600

    give redux a try

--- part6/counter-app/src/main.jsx
+++ part6/counter-app/src/main.jsx
@@ -1,10 +1,45 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import { createStore } from "redux";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+const App = () => {
+  return (
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={(e) => store.dispatch({ type: "INCREMENT" })}>
+        plus
+      </button>
+      <button onClick={(e) => store.dispatch({ type: "DECREMENT" })}>
+        minus
+      </button>
+      <button onClick={(e) => store.dispatch({ type: "ZERO" })}>zero</button>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
