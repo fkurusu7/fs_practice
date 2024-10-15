ae84dcd6a4e9efcec1927082a3d34886408168f6
fkurusu7 <coding.fcv@gmail.com>
2024-10-15 14:53:58 -0600

    add react-query

--- part6/query-notes/src/App.jsx
+++ part6/query-notes/src/App.jsx
@@ -1,32 +1,69 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getNotes, createNote, updateNote } from "./requests";
+
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const newNotemutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(["notes"]);
+      queryClient.setQueryData(["notes"], notes.content(newNote));
+    },
+  });
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (updatedNote) => {
+      queryClient.setQueryData(["notes"], (oldNotes) => {
+        return oldNotes.map((note) =>
+          note.id === updateNote.id ? updatedNote : note
+        );
+      });
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
   const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    // console.log(content);
+    newNotemutation.mutate({ content, important: true });
+  };
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({ ...note, important: !note.important });
+  };
+
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false,
+  });
+  // console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
   }
 
-  const notes = []
+  const notes = result.data;
 
-  return(
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
